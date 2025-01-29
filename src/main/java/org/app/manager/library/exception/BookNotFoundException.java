ac2f64240530b424e6939a2d55575ed8f4a9c4a9
JonathanHTran <tr.h.jonathan@gmail.com>
2025-01-29 13:04:57 +0900

    reset

--- src/main/java/org/app/manager/library/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.app.manager.library.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/app/manager/library/exception/BorrowRecordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.app.manager.library.exception;
-
-public class BorrowRecordNotFoundException extends RuntimeException {
-    public BorrowRecordNotFoundException(String message) {
-        super(message);
-    }
-}
